3ae0db38c56d86c6fe2ec42bd50c147a2b264c33
Jose Seie <joseeugenioseie@hotmail.com>
2017-07-31 17:03:47 +0200

    Estrutura basica criada

---
+++ EquipaDeFutebol/src/equipadefutebol/EquipaDeFutebol.java
@@ -0,0 +1,17 @@
+
+package equipadefutebol;
+
+/**
+ *
+ * @author José Seie
+ */
+public class EquipaDeFutebol {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    
+}
